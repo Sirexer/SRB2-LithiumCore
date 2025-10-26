e3534f1da40bf437e1e80945765b47feab1663d9
Sirexer <85247518+Sirexer@users.noreply.github.com>
2025-10-26 19:02:14 +0300

    Create LC_MenuAction.lua

---
+++ lua/2-Constants/LC_MenuAction.lua
@@ -0,0 +1,11 @@
+-- ==========================
+-- Menu action constants
+-- ==========================
+rawset(_G, "LCMA_NAVLEFT",	1)
+rawset(_G, "LCMA_NAVRIGHT",	2)
+rawset(_G, "LCMA_NAVUP",		3)
+rawset(_G, "LCMA_NAVDOWN",	4)
+rawset(_G, "LCMA_ACCEPT",		5)
+rawset(_G, "LCMA_BACK",		6)
+
+return true -- End of File
